76d8f4c458db52121a5228b148653396f59079fb
Michael Hicks <hicks.michael04@gmail.com>
2023-06-06 17:07:07 -0600

    Feat: build out products stylesheet format

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.delete_all 
+
+Product.create!(title: 'Build Chatbot Interactions', 
+    description:
+        %{
+        Responsive, Intuitive Interfaces with Ruby
+        The next step in the evolution of user interfaces is here. 
+        Chatbots let your users interact with your service in their own natural language. 
+        Use free and open source tools along with Ruby to build creative, useful, and unexpected interactions for users.
+        },
+    image_url: 'dpchat.jpg',
+    price: 20.00
+    )
