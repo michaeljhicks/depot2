c7c52f514e437779ddf70c87b9525c422595509a
Michael Hicks <hicks.michael04@gmail.com>
2023-06-10 19:15:54 -0600

    Feat: create up method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_11_005918) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_11_011000) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
